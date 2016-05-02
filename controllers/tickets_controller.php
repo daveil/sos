421020221d88e838990f3323dfa826330950413a
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-05-02 14:24:15 +0800

    Adjust ticket controller

--- controllers/tickets_controller.php
+++ controllers/tickets_controller.php
@@ -4,8 +4,13 @@ class TicketsController extends AppController {
 	var $name = 'Tickets';
 
 	function index() {
-		$this->Ticket->recursive = 0;
-		$this->set('tickets', $this->paginate());
+		if($this->RequestHandler->isAjax()){
+			$response = $this->Ticket->find('all',array('order'=>'modified DESC','limit'=>3));
+			echo json_encode($response);exit;
+		}else{
+			$this->Ticket->recursive = 0;
+			$this->set('tickets', $this->paginate());			
+		}
 	}
 
 	function view($id = null) {
@@ -13,15 +18,24 @@ class TicketsController extends AppController {
 			$this->Session->setFlash(__('Invalid ticket', true));
 			$this->redirect(array('action' => 'index'));
 		}
-		$this->set('ticket', $this->Ticket->read(null, $id));
+		$this->set('ticket', $ticket =  $this->Ticket->read(null, $id));
+		
+		if($this->RequestHandler->isAjax()){
+			echo json_encode($ticket);exit;
+		}
 	}
 
 	function add() {
 		if (!empty($this->data)) {
 			$this->Ticket->create();
 			if ($this->Ticket->save($this->data)) {
-				$this->Session->setFlash(__('The ticket has been saved', true));
-				$this->redirect(array('action' => 'index'));
+				if($this->RequestHandler->isAjax()){
+					$response = array('status'=>'OK','message'=>'Data saved');
+					$this->redirect(array('action' => 'view',$this->Ticket->id));
+				}else{
+					$this->Session->setFlash(__('The ticket has been saved', true));
+					$this->redirect(array('action' => 'index'));
+				}
 			} else {
 				$this->Session->setFlash(__('The ticket could not be saved. Please, try again.', true));
 			}
